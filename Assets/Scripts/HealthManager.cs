9ea542e4e7abdd88f38b14d8ac0e2c0980b70fdd
annalissa17 <annalissa.laroche15@gmail.com>
2024-11-26 11:28:16 -0500

    health bar completement fonctionelle

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -7,14 +7,33 @@ using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
 {
     public BarreDeVie barreDeVie;
-    [SerializeField] private Image[] currentHealth;
+    [SerializeField] private GameObject[] currentHealth;
 
     private void Start()
     {
         MaxHealth(25f);
     }
-    public void MaxHealth(float maxHealth){
+    private void Update()
+    {
+        CheckHealth();
+    }
+    private void MaxHealth(float maxHealth){
         maxHealth = barreDeVie._maxHealth;
         Debug.Log("max health: " + maxHealth);
+    }
+
+    void CheckHealth(){
+        for( int i = 0; i <= barreDeVie._maxHealth; i++ ){
+            UpdateHealth();
+            Debug.Log(i);
+            currentHealth[i].SetActive(true);
+            
+        }
+    }
+
+    void UpdateHealth(){
+        foreach(GameObject frames in currentHealth){
+            frames.SetActive(false);
+        }
     }
 }
